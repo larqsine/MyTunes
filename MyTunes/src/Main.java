92b2c147adb46c9e0f1382f3c4164fc333b13ad9
Radwan <145333687+larqsine@users.noreply.github.com>
2023-11-27 11:55:45 +0100

    New window

--- MyTunes/src/Main.java
+++ MyTunes/src/Main.java
@@ -1,7 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     int Number = 1;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Fxml.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
