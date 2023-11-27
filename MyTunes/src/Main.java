ac1d57f00448f9be9630ec46d3f2bee1dad6b15b
YehorNe1 <yehornechai1@gmail.com>
2023-11-27 11:44:14 +0100

    Update Main.java

--- MyTunes/src/Main.java
+++ MyTunes/src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello guys I am Yehor ");
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
