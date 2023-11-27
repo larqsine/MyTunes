cdf3e41dd8d888ad446d111a63d2840261e383dc
YehorNe1 <yehornechai1@gmail.com>
2023-11-27 12:11:49 +0100

    change

--- MyTunes/src/Main.java
+++ MyTunes/src/Main.java
@@ -1,14 +1,21 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello guys I am Yehor ");
-        
+        launch(args);
+
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
